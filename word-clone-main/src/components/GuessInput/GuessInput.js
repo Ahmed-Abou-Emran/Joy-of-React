a67c56a570cbf4a65e52f22c4039f2968a762aa7
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-02-05 08:13:04 +0200

    Keeping track of guesses

--- word-clone-main/src/components/GuessInput/GuessInput.js
+++ word-clone-main/src/components/GuessInput/GuessInput.js
@@ -1,16 +1,21 @@
 import React from "react";
 
-function GuessInput() {
+function GuessInput({ setGuesses }) {
   const [guess, setGuess] = React.useState("");
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    if (guess.length !== 5) {
+      window.alert("Guess must be 5 letters long.");
+      return;
+    }
+    setGuesses((prevGuesses) => [...prevGuesses, guess]);
     console.log({ guess });
     setGuess("");
   };
   return (
     <form onSubmit={onSubmitHandler} className="guess-input-wrapper">
-      <label forHtml="guess-input">Enter guess:</label>
+      <label htmlFor="guess-input">Enter guess:</label>
       <input
         required
         onChange={(event) => setGuess(event.target.value.toUpperCase())}
