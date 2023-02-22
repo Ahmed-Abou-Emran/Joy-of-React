fed300e3cb2724376544875297f6c8b0d4ce4054
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-02-22 13:12:06 +0200

    Ex2: Live-editable toast preview

--- toast-component-main/src/components/ToastPlayground/ToastPlayground.js
+++ toast-component-main/src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../Button";
+import Toast from "../Toast/Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -8,7 +9,9 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
-  const [variantSelected, setVariantSelected] = React.useState("notice");
+  const [variantSelected, setVariantSelected] = React.useState(
+    VARIANT_OPTIONS[0]
+  );
 
   const onMessageChange = (event) => {
     setMessage(event.target.value);
@@ -18,6 +21,15 @@ function ToastPlayground() {
     setVariantSelected(event.target.value);
   };
 
+  const [toasstStatus, setToastStatus] = React.useState(false);
+
+  const showToast = () => {
+    setToastStatus(true);
+  };
+  const hideToast = () => {
+    setToastStatus(false);
+  };
+
   console.log({ message, variantSelected });
   return (
     <div className={styles.wrapper}>
@@ -25,7 +37,15 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-
+      {toasstStatus && (
+        <Toast
+          variant={variantSelected}
+          hideToast={hideToast}
+          toasstStatus={toasstStatus}
+        >
+          {message}
+        </Toast>
+      )}
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
@@ -52,7 +72,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={showToast}>Pop Toast!</Button>
           </div>
         </div>
       </div>
