9d37191b6233fba75793394e36cb1a0d24c5c6ba
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-02-25 18:39:42 +0200

    Ex5.1: Support Keyboard users

--- toast-component-main/src/components/ToastShelf/ToastShelf.js
+++ toast-component-main/src/components/ToastShelf/ToastShelf.js
@@ -5,7 +5,21 @@ import styles from "./ToastShelf.module.css";
 
 import { ToastContext } from "../ToastProvider";
 function ToastShelf() {
-  const { toasts, filterToasts } = React.useContext(ToastContext);
+  const { toasts, filterToasts, clearToastMessage } =
+    React.useContext(ToastContext);
+
+  React.useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        clearToastMessage();
+      }
+
+      window.addEventListener("keydown", handleKeyDown);
+    };
+
+    return () => window.addEventListener("keydown", handleKeyDown);
+  }, [clearToastMessage]);
+
   return (
     <ol className={styles.wrapper}>
       {toasts.map(({ variantSelected, message, id }, index) => (
