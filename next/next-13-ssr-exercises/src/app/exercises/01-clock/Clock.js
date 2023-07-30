c13e7ffb963b51eabd807c1ac6c35971f9c759dd
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-07-30 17:37:50 +0300

    updating ssr exercises

--- next/next-13-ssr-exercises/src/app/exercises/01-clock/Clock.js
+++ next/next-13-ssr-exercises/src/app/exercises/01-clock/Clock.js
@@ -1,11 +1,13 @@
-'use client';
-import React from 'react';
-import format from 'date-fns/format';
+"use client";
+import React from "react";
+import format from "date-fns/format";
 
 function Clock() {
+  const [isClient, setIsClient] = React.useState(false);
   const [time, setTime] = React.useState(new Date());
 
   React.useEffect(() => {
+    setIsClient(true);
     const intervalId = window.setInterval(() => {
       setTime(new Date());
     }, 50);
@@ -16,7 +18,9 @@ function Clock() {
   }, []);
 
   return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
+    <p className="clock">
+      {isClient ? format(time, "HH:mm:ss") : "Loading..."}
+    </p>
   );
 }
 
